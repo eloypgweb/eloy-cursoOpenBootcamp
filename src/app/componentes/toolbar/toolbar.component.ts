48501f5eb478dcd4095cec673627cb53b0705644
eloy <titoeloc@gmail.com>
2024-04-02 17:45:27 +0200

    PrimerosPasos v1.0.2

--- src/app/componentes/toolbar/toolbar.component.ts
+++ src/app/componentes/toolbar/toolbar.component.ts
@@ -2,14 +2,31 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatBadgeModule} from '@angular/material/badge';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatDividerModule, MatBadgeModule, RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    console.log('ToolbarComponent initialized');
+  }
+
+  navegarParaContacts(): void {
+    this.router.navigate(['contacts']);
+  }
+  navegarParaHome(): void {
+    this.router.navigate(['/']);
+  }
 
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
