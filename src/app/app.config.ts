1c0c21efb0126d19453e1d1a5fa4e4cacad535d8
eloy6lega <eloy.perez@campusfp.es>
2024-04-03 14:27:18 +0200

    PrimerosPasos v1.0.4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), NgModel]
 };
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,17 +3,22 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
-  constructor(private router: Router) { }
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
     let token = sessionStorage.getItem('token');
@@ -23,6 +28,19 @@ export class LoginPageComponent {
   }
 
   loginUser() {
+
+    this.authService.login(this.email, this.password).subscribe(
+      (response) => {
+        if (response.token) {
+          sessionStorage.setItem('token', response.token);
+          this.router.navigate(['contacts']);
+        }
+      },
+      (error) => console.log('Ha habido un error: ',error),
+      () => console.log('Petición completa')
+    )
+
+
     sessionStorage.setItem('token', '123456789');
     this.router.navigate(['contacts']);
   }
